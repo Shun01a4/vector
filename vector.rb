d717c6250e20c72f5c49c114ea3a26f78cf73b2e
Shun <j1223@PC4-54.cc.matsue-ct.jp>
2017-09-26 11:21:15 +0900

    add method update

--- vector.rb
+++ vector.rb
@@ -3,10 +3,16 @@ class Vector
 		@x=x
 		@y=y
 	end
+	attr_accessor :x
+	attr_accessor :y
 	def to_s
 		return "(#{@x},#{@y})"
 	end
 	def length
 		return Math::sqrt(@x*@x+@y+@y)
+	end
+	def add(v2)
+		@v3=Vector.new(@x+v2.x,@y+v2.y)
+		return @v3
 	end
 end
